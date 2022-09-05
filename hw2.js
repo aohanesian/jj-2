2bf0635d9d07a70967aef4f0de4558d2d6579a08
aohanesian <103946712+aohanesian@users.noreply.github.com>
2022-09-05 17:16:31 +0300

    init

---
+++ hw2.js
@@ -0,0 +1,19 @@
+`use strict`;
+
+// task 1
+
+let admin;
+const name = `Джон`;
+admin = name;
+alert(admin);
+
+//task 2
+
+const homePlanet = `Earth`;
+let userName = `Alex`;
+
+//task 3
+
+let math = `10` + 10;
+console.log(math);
+//конкатенация строки и числа
